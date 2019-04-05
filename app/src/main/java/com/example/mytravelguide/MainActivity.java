7f086882fb8e768c16f0e1f2a1c7bb71d60090e6
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-04-05 17:33:31 +0100

    Added Sign In Activity, Splash screen goes to Sign In

--- app/src/main/java/com/example/mytravelguide/MainActivity.java
+++ app/src/main/java/com/example/mytravelguide/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mytravelguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -17,10 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
+        setUpAnimation();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // yourMethod();
+                Intent signInIntent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(signInIntent);
+            }
+        }, 10000);   //5 seconds
+
+
+    }
+
+    private void setUpAnimation() {
         companyLogo = findViewById(R.id.company_logo);
-
         fromTop = AnimationUtils.loadAnimation(this, R.anim.fromtop);
-
         companyLogo.setAnimation(fromTop);
     }
 }
