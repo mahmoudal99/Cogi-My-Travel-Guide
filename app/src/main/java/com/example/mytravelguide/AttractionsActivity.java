3bda5cec855d8920a23933c7ef8fcfe2d1488dcc
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-04-07 16:54:38 +0100

    Added Continent Images to Attractions Page

--- app/src/main/java/com/example/mytravelguide/AttractionsActivity.java
+++ app/src/main/java/com/example/mytravelguide/AttractionsActivity.java
@@ -1,14 +1,21 @@
 package com.example.mytravelguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AttractionsActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attractions);
     }
+
+
 }
--- app/src/main/java/com/example/mytravelguide/HomePageActivity.java
+++ app/src/main/java/com/example/mytravelguide/HomePageActivity.java
@@ -1,14 +1,36 @@
 package com.example.mytravelguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    CardView attractionsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        init();
+        setUpWidgets();
+    }
+
+    private void init(){
+        attractionsCard = findViewById(R.id.attractionsCard);
+    }
+
+    private void setUpWidgets(){
+        attractionsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent attractionsIntent = new Intent(HomePageActivity.this, AttractionsActivity.class);
+                startActivity(attractionsIntent);
+            }
+        });
     }
 }
