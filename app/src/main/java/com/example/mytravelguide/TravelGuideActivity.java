cbb95cbe7e555596bf72199488520aea7c3d063c
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-04-07 17:15:58 +0100

    Added Travel Guide Activity

---
+++ app/src/main/java/com/example/mytravelguide/TravelGuideActivity.java
@@ -0,0 +1,14 @@
+package com.example.mytravelguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TravelGuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_guide);
+    }
+}
