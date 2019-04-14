6bb2fdcaf9d1cf3a4411b59b6aa3da98e44cf1cf
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-04-14 12:00:12 +0100

    Add visited places to firestore database

--- app/src/main/java/com/example/mytravelguide/TravelGuideActivity.java
+++ app/src/main/java/com/example/mytravelguide/TravelGuideActivity.java
@@ -1,17 +1,41 @@
 package com.example.mytravelguide;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TravelGuideActivity extends AppCompatActivity{
 
+    private static final String TAG = "TravelGuideActivity";
+
+    // Widgets
     ImageView backArrow, addPlace;
 
     VisitedActivity visitedActivity;
+
+    // Firebase
+    private FirebaseAuth authentication;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +44,7 @@ public class TravelGuideActivity extends AppCompatActivity{
 
         init();
         setUpWidgets();
+        setUpFirebaseAuthentication();
     }
 
     private void init(){
@@ -40,8 +65,95 @@ public class TravelGuideActivity extends AppCompatActivity{
         addPlace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                visitedActivity.addPlaceToTimeline("Paris", "10/4/2019");
+                addVisitedPlace("Paris");
             }
         });
     }
+
+    private void addVisitedPlace(String name){
+        // Create a new user with a first and last name
+        Map<String, String> place = new HashMap<>();
+        place.put("Place Name", "Petra");
+        place.put("Date Visited", "April 2019");
+
+        // Access a Cloud Firestore instance from your Activity
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        // Add a new document with a generated ID
+        db.collection("VisitedPlaces").document(currentUser.getUid()).collection("MyPlaces").add(place)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: ");
+                    }
+                });
+
+    }
+
+    //---------- Firebase ----------//
+    private void setUpFirebaseAuthentication() {
+        authentication = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                if (currentUser != null) {
+                    Log.d(TAG, "Success");
+                } else {
+                    Log.d(TAG, "signed out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        authentication.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            authentication.removeAuthStateListener(authStateListener);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
