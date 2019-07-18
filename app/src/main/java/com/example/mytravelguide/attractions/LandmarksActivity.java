53f597d582a96b97c626873fcf4ba40cf4f84b9b
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-07-18 15:26:30 +0100

    Added recycler view to discovery layout

--- app/src/main/java/com/example/mytravelguide/attractions/LandmarksActivity.java
+++ app/src/main/java/com/example/mytravelguide/attractions/LandmarksActivity.java
@@ -31,7 +31,9 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
 public class LandmarksActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -75,7 +77,7 @@ public class LandmarksActivity extends AppCompatActivity implements OnMapReadyCa
     }
 
     private void getCityDataId(String cityName) {
-        String url = "https://wft-geo-db.p.mashape.com/v1/geo/cities?namePrefix=" + cityName + "&minPopulation=1000000";
+        String url = "https://wft-geo-db.p.mashape.com/v1/geo/cities?namePrefix="+cityName+"&minPopulation=1000000";
         Request cityDataIDRequest = new Request.Builder()
                 .url(url)
                 .header("X-RapidAPI-Host", "wft-geo-db.p.rapidapi.com")
@@ -86,16 +88,20 @@ public class LandmarksActivity extends AppCompatActivity implements OnMapReadyCa
 
     private void getCityLandmarks(String cityWikiDataID){
         String cityDataId = cityWikiDataID;
+        String[] types = {"Q16560", "Q12518", "Q23413", "Q570116", "Q839954"};
         String typePalace = "Q16560";
         String typeTower = "Q12518";
         String typeCastle = "Q23413";
         String typeTouristAttraction = "Q570116";
         String typeArchaeologicalSite = "Q839954";
 
-        String url1 = "https://query.wikidata.org/sparql?format=json&query=%0ASELECT+DISTINCT+%3Fitem+%3Fname+%3Fcoord+%3Flat+%3Flon%0AWHERE+%7B%0A+++hint%3AQuery+hint%3Aoptimizer+%22None%22+.%0A+++%3Fitem+wdt%3AP131%2A+wd%3A"+cityDataId+"+.%0A+++%3Fitem+wdt%3AP31%2Fwdt%3AP279%2A+wd%3A"+typePalace+"+.%0A+++%3Fitem+wdt%3AP625+%3Fcoord+.%0A+++%3Fitem+p%3AP625+%3Fcoordinate+.%0A+++%3Fcoordinate+psv%3AP625+%3Fcoordinate_node+.%0A+++%3Fcoordinate_node+wikibase%3AgeoLatitude+%3Flat+.%0A+++%3Fcoordinate_node+wikibase%3AgeoLongitude+%3Flon+.%0A+++SERVICE+wikibase%3Alabel+%7B%0A++++bd%3AserviceParam+wikibase%3Alanguage+%22%5BAUTO_LANGUAGE%5D%2Cen%22+.%0A++++%3Fitem+rdfs%3Alabel+%3Fname%0A+++%7D%0A%7D%0AORDER+BY+ASC+%28%3Fname%29%0A";
-        Request cityLandmarksRequest = new Request.Builder().url(url1).header("content-type", "application/html").build();
+        for (int i = 0; i < 5; i++){
+            String url1 = "https://query.wikidata.org/sparql?format=json&query=%0ASELECT+DISTINCT+%3Fitem+%3Fname+%3Fcoord+%3Flat+%3Flon%0AWHERE+%7B%0A+++hint%3AQuery+hint%3Aoptimizer+%22None%22+.%0A+++%3Fitem+wdt%3AP131%2A+wd%3A"+cityDataId+"+.%0A+++%3Fitem+wdt%3AP31%2Fwdt%3AP279%2A+wd%3A"+types[i]+"+.%0A+++%3Fitem+wdt%3AP625+%3Fcoord+.%0A+++%3Fitem+p%3AP625+%3Fcoordinate+.%0A+++%3Fcoordinate+psv%3AP625+%3Fcoordinate_node+.%0A+++%3Fcoordinate_node+wikibase%3AgeoLatitude+%3Flat+.%0A+++%3Fcoordinate_node+wikibase%3AgeoLongitude+%3Flon+.%0A+++SERVICE+wikibase%3Alabel+%7B%0A++++bd%3AserviceParam+wikibase%3Alanguage+%22%5BAUTO_LANGUAGE%5D%2Cen%22+.%0A++++%3Fitem+rdfs%3Alabel+%3Fname%0A+++%7D%0A%7D%0AORDER+BY+ASC+%28%3Fname%29%0A";
+            Request cityLandmarksRequest = new Request.Builder().url(url1).header("content-type", "application/html").build();
 
-        httpClientCall(cityLandmarksRequest, "LANDMARKSREQEST");
+            httpClientCall(cityLandmarksRequest, "LANDMARKSREQEST");
+        }
+
     }
 
     private void httpClientCall(Request request, String requestType) {
@@ -126,7 +132,12 @@ public class LandmarksActivity extends AppCompatActivity implements OnMapReadyCa
             JSONArray jsonArray = jsonObject.getJSONArray("bindings");
             for (int i = 0; i < jsonArray.length(); i++){
                 JSONObject finalObject = jsonArray.getJSONObject(i);
-                Log.d("landmarksInCityFromJson", finalObject.get("name") + " jodjo");
+                Set<String> landmarks = new HashSet<>();
+
+                jsonObject = new JSONObject(finalObject.getString("name"));
+
+                landmarks.add(jsonObject.get("value").toString());
+                landmarksInCity(landmarks);
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -142,7 +153,12 @@ public class LandmarksActivity extends AppCompatActivity implements OnMapReadyCa
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
 
+    private void landmarksInCity(Set<String> landmarks){
+        for (String landmark : landmarks){
+            Log.d("LANDMARKINPARIS", landmark);
+        }
     }
 
     private void setUpWidgets() {
