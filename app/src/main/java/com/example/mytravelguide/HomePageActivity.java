f9ce48a3b0411b53b5d52365495f32c26f8548b3
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-09-22 15:32:40 +0100

    Fixed google ad position

--- app/src/main/java/com/example/mytravelguide/HomePageActivity.java
+++ app/src/main/java/com/example/mytravelguide/HomePageActivity.java
@@ -17,6 +17,13 @@ import android.util.Log;
 import com.example.mytravelguide.attractions.CitiesActivity;
 import com.example.mytravelguide.models.ImageModel;
 import com.example.mytravelguide.utils.SlidingImageAdapter;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -51,6 +58,8 @@ public class HomePageActivity extends AppCompatActivity {
 
     private String[] imageNames = new String[]{"Giza Necropolis", "Jerusalem", "Petra", "Rome"};
 
+    AdView mAdView;
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +73,51 @@ public class HomePageActivity extends AppCompatActivity {
         setUpWidgets();
         setUpFirebaseAuthentication();
         initViewPager();
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Log.d("Google Ad MOb", "Loaded");
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
     }
 
     private ArrayList<ImageModel> populateList() {
